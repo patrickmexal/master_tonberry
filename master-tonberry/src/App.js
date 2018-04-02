945f113c551ef94d96b169c0942cb579bed990d9
Patrick Mexal <patrickmexal@gmail.com>
2018-04-01 19:37:32 -0700

    added touches

--- master-tonberry/src/App.js
+++ master-tonberry/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Sidebar from "./components/Sidebar.js";
 import Header from "./components/Header.js";
+import Fill from "./components/Fill.js";
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       <Header />
         <Sidebar />
+        <Fill />
         </div>
     );
   }
